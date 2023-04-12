42f417245c6da733b4063439c508c9f85fee8ef5
Nrittik Sarmah <nrittik43@gmail.com>
2023-04-12 22:16:49 +0530

    added gitignore

--- backend/services/codes/6b52b6ca-abb9-49e3-a70a-37fb5ea9e282.java
+++
@@ -1,7 +0,0 @@
-// Class with the main function should be named "HelloWorld"
-class HelloWorld{ 
-
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
