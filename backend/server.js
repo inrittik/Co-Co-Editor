f3736e524bc193588ea942d1e084783f00a2c977
Nrittik Sarmah <nrittik43@gmail.com>
2023-04-12 19:49:22 +0530

    execution time

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,53 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const dotenv = require('dotenv');
-const path = require('path');
-const { ACTIONS } = require('./actions')
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const dotenv = require("dotenv");
+const path = require("path");
+const { ACTIONS } = require("./actions");
 const { generateFile } = require("./services/generateFile");
-const { cCodeRunner, cppCodeRunner, javaCodeRunner, pythonCodeRunner, jsCodeRunner } = require('./services/codeRunner');
+const {
+  cCodeRunner,
+  cppCodeRunner,
+  javaCodeRunner,
+  pythonCodeRunner,
+  jsCodeRunner,
+} = require("./services/codeRunner");
 
 dotenv.config({ path: path.join(__dirname, `./.env`) });
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server)
+const io = new Server(server);
 
-io.on('connection', (socket) => {
-    console.log(socket.id)
+io.on("connection", (socket) => {
+  socket.on(ACTIONS.RUN, async ({ code, extension = "c" }) => {
+    try {
+      const filePath = await generateFile(code, extension);
+      let output;
+      const startedAt = new Date();
+      if (extension === "c") output = await cCodeRunner(filePath);
+      else if (extension === "cpp") output = await cppCodeRunner(filePath);
+      else if (extension === "java") output = await javaCodeRunner(filePath);
+      else if (extension === "py") output = await pythonCodeRunner(filePath);
+      else if (extension === "js") output = await jsCodeRunner(filePath);
 
-    socket.on(ACTIONS.RUN, async ({code, extension="c"}) => {
-        try {
-            const filePath = await generateFile(code, extension);
-            let output
-            if (extension === "c") output = await cCodeRunner(filePath);
-            else if (extension === "cpp")
-              output = await cppCodeRunner(filePath);
-            else if (extension === "java")
-              output = await javaCodeRunner(filePath);
-            else if (extension === "py")
-              output = await pythonCodeRunner(filePath);
-            else if (extension === "js")
-              output = await jsCodeRunner(filePath);
-            console.log(output)
-            io.to(socket.id).emit(ACTIONS.RETURN, {
-                output,
-                success: true,
-            })
-        }
-        catch (err) {
-            io.to(socket.id).emit(ACTIONS.RETURN, {
-                output: err,
-                success: false
-            });
-        }
-    })
-})
-
+      const endedAt = new Date();
+      io.to(socket.id).emit(ACTIONS.RETURN, {
+        output,
+        success: true,
+        startedAt,
+        endedAt,
+      });
+    } catch (err) {
+      io.to(socket.id).emit(ACTIONS.RETURN, {
+        output: err,
+        success: false,
+      });
+    }
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-})
+  console.log(`Listening on port ${PORT}`);
+});
---
+++ backend/services/codes/8acb80bc-6a8c-4fd0-b87b-dfdfd4b7368c.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+    
+int main(){
+    printf("Hello World!\n123");
+    return 0;
+}
